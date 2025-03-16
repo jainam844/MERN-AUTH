d438e6339dc1410b7b739d66e7e0eb1ca085281f
JAINAM SHAH <shahjainam491@gmail.com>
2025-03-16 14:09:08 +0530

    sign up completed

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,10 @@ import authRoutes from "./routes/auth.route.js"
 
 dotenv.config()
 const app = express();
+
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json())
 
 app.use("/api/auth", authRoutes)
 
