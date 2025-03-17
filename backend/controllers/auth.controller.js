98666a357120e8b88423f496a25a35c21a9cc1ad
JAINAM SHAH <shahjainam491@gmail.com>
2025-03-17 23:08:09 +0530

    reset started

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -109,7 +109,6 @@ export const login = async (req, res) => {
     }
 }
 
-
 export const forgotPassword = async (req, res) => {
     const { email } = req.body;
     try {
@@ -118,6 +117,29 @@ export const forgotPassword = async (req, res) => {
             return res.status(400).json({ success: false, message: "Invalid credentials" });
         }
 
+        const resetToken = crypto.randomBytes(20).toString("hex")
+        const resetTokenExpiresAt = Date.now() + 1 * 60 * 60 * 1000; // 1 hour
+
+        user.resetPasswordToken = resetToken;
+        user.resetPasswordExpiresAt = resetTokenExpiresAt;
+
+        await user.save();
+        await sendPasswordResetEmail(user.email, `${process.env.CLIENT_URL}/reset-password/${resetToken}`);
+        res.status(200).json({ success: true, message: "Password reset link sent to your email" });
+
+    } catch (error) {
+        console.log("Error in login ", error);
+        res.status(400).json({ success: false, message: error.message });
+    }
+}
+
+export const resetPassword = async (req, res) => {
+    const { email } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Invalid credentials" });
+        }
 
         const resetToken = crypto.randomBytes(20).toString("hex")
         const resetTokenExpiresAt = Date.now() + 1 * 60 * 60 * 1000; // 1 hour
